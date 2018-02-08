dd718e3a172e80f008db0b5b3517f3ae7caf4032
Paul Johns <35434386+Johnny-Codes@users.noreply.github.com>
2018-02-08 13:07:45 -0500

    Update nflcombine.py
    Should be the final version.
    
    Major updates:
    Gather's Header data once and writes to file
    Returns empty cells if there's no data
    Created a function to customize the years to scrape

--- nflcombine.py
+++ nflcombine.py
@@ -3,89 +3,96 @@ import requests
 import re
 import csv
 
-url_list = [
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1987&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1988&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1989&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1990&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1991&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1992&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1993&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1994&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1995&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1996&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1997&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1998&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=1999&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2000&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2001&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2002&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2003&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2004&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2005&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2006&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2007&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2008&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2009&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2010&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2011&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2012&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2013&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2014&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2015&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2016&pos=&college=",
-"http://nflcombineresults.com/nflcombinedata_expanded.php?year=2017&pos=&college="
-]
+url_list = []
 
-#r = requests.get(url_list[0])
+### Can choose which years you want to scrape for
+### Add one year to end (ex: range(1987-1990) will search 1987, 88, and 89)
+for num in range(1987, 2018):
+    url_list.append(f"http://nflcombineresults.com/nflcombinedata_expanded.php?year={num}&pos=&college=")
 
-filename = "playerdata2.csv"
+print(url_list)
+
+filename = "playercombinedata.csv"
 
 def openURL(input_list):
-# Added a list to store the data from different pages
+### Added a list to store the data from different pages
     new_list = []
+
     for url in input_list:
         r = requests.get(url)
         page_text = bs(r.content, 'html.parser')
-        # Appending the data to this list
         new_list.append(page_text)
-    # print(new_list)
     return new_list
 
+def getHeaderData(soup):
+### This function identifies the wanted table within the given and passed webpage from function openURL()
+### After identification with beautifulsoup, it will load the table into the multidim. list data[]
+    table = soup.find('table')
+    table_header = table.find('thead')
+
+    data = []
+
+    rows = table_header.find_all('tr')
+
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols])
+
+    for row in data:
+        for elem in row:
+            return data
+
 def getPlayerData(soup):
-# This function identifies the wanted table within the given and passed webpage from function openPage()
-# After identification with beautiful soup, it will loads the table into the multidim. list data[]
+### This function identifies the wanted table within the given and passed webpage from function openURL()
+### After identification with beautifulsoup, it will load the table into the multidim. list data[]
     table = soup.find('table')
     table_body = table.find('tbody')
 
     data = []
+
     rows = table_body.find_all('tr')
+
     for row in rows:
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele])
+        data.append([ele for ele in cols])
 
     for row in data:
         for elem in row:
             return data
 
 def ExportToCSV(data):
-    # Added a parameter to remove the newlines in the output file
+    ### Added a parameter to remove the newlines in the output file
     with open(filename, 'w', newline = '') as f:
         writer = csv.writer(f, delimiter=',')
         for row in data:
             # Writing to the csv column wise
             for column in row:
                 writer.writerow(column)
-        # writer.writerows(data)
-    print('Player Data data variable has been saved to file', filename)
 
 soup = openURL(url_list)
-# List to store the data from multiple pages
+
+### List to store the data from multiple pages
 multi_pages = []
-# For the list items in the list
+
+### For the list items in the list
+### ExportToCSV
+
+### This gets the header data only once
+get_header_data = 0
+for li in soup:
+    get_header_data = get_header_data + 1
+    if get_header_data == 1:
+        header_data = getHeaderData(li)
+        multi_pages.append(header_data)
+    else:
+        break
+
+ExportToCSV(multi_pages)
+
+### This gets the players and their data
 for li in soup:
     player_data = getPlayerData(li)
     multi_pages.append(player_data)
 ExportToCSV(multi_pages)
-# ExportToCSV(player_data)
